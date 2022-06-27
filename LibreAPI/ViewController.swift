89d3a8bd24ad3414b1821bfd14c2978ab1ab7db0
KCommit <kcommitdev@gmail.com>
2022-06-27 11:44:30 +0800

    finish HttpRequestBar

--- LibreAPI/ViewController.swift
+++ LibreAPI/ViewController.swift
@@ -1,18 +1,35 @@
-//
-//  ViewController.swift
-//  LibreAPI
-//
-//  Created by KCommit on 2022/6/27.
-//
-
 import Cocoa
+import SnapKit
 
 class ViewController: NSViewController {
+    let httpRequestMethods = [
+        "GET"
+    ]
+    
+    let stackView = NSStackView()
+    let httpMethodPicker = NSPopUpButton()
+    let urlTextField = NSTextField()
+    let sendRequestButton = NSButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.left.equalTo(view.snp.left).offset(10)
+            make.top.equalTo(view.snp.top).offset(10)
+            make.right.equalTo(view.snp.right).offset(-10)
+        }
+        
+        httpMethodPicker.addItems(withTitles: httpRequestMethods)
+        stackView.addArrangedSubview(httpMethodPicker)
+        
+        urlTextField.maximumNumberOfLines = 1
+        stackView.addArrangedSubview(urlTextField)
+        
+        sendRequestButton.title = "Send"
+        sendRequestButton.bezelStyle = .rounded
+        stackView.addArrangedSubview(sendRequestButton)
     }
 
     override var representedObject: Any? {
